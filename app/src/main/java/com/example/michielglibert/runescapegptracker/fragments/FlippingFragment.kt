1289d17297a47291ef161f656ae67d70c2c3cd49
Michiel Glibert <michiel.glibert@gmail.com>
2018-01-21 21:42:52 +0100

    Initial commit
    - Made navigation drawer
    - Made fragment

---
+++ app/src/main/java/com/example/michielglibert/runescapegptracker/fragments/FlippingFragment.kt
@@ -0,0 +1,39 @@
+package com.example.michielglibert.runescapegptracker.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.michielglibert.runescapegptracker.R
+
+class FlippingFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater!!.inflate(R.layout.fragment_flipping, container, false)
+
+        return view
+    }
+
+    /*override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            mListener = context
+        } else {
+            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }*/
+}
